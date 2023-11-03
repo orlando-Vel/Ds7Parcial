bdb683da993589f80b9981da363cb5943f96e3f3
Orlando Velásquez <orlando.a06@gmail.com>
2023-11-02 21:24:15 -0500

    primer commit

---
+++ view/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="div-formulario">
+        <img src="..\logo\logoutp.png" alt="" class="logo">
+        <form method="post" action="..\controller\validar.php">
+            <h1>Sistema de Login</h1>
+            <p>Email: <input type="text" placeholder="Ingrese su Email" name="email"></p>
+            <p>Comtraseña: <input type="password" placeholder="Ingrese su contraseña" name="pass"></p>
+            <input type="submit" value="Ingresar" class="btnEnviar">
+            <p>No tienes cuaenta? <a href="crearUsuario.php">Crear cuenta</a></p>
+        </form>
+    </div>
+</body>
+</html>
